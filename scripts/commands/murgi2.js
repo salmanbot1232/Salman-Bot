dda5af1c13cd93005a492e165f02165186a1d7a3
salmanbot1232 <githubsalman@gmail.com>
2025-06-07 14:04:55 +0600

    Update murgi2.js

--- scripts/commands/murgi2.js
+++ scripts/commands/murgi2.js
@@ -17,7 +17,7 @@ module.exports.run = async function({ api, args, Users, event}) {
     var arraytag = [];
         arraytag.push({id: mention, tag: name});
     var a = function (a) { api.sendMessage(a, event.threadID); }
-a("রকির ইন্ডিয়ান চুদা লো");
+a("তোর আব্বু সালমানের চুদা লো");
 setTimeout(() => {a({body: "খাংকির পোলা তর মারে চুদি 🥰।" + " " + name, mentions: arraytag})}, 3000);
 setTimeout(() => {a({body: "খাংকির পোলা তর কচি বোন রে চুদি 😍.." + " " + name, mentions: arraytag})}, 5000);
 setTimeout(() => {a({body: "মাদারচোদ তর আম্মু পম পম খাংকির পো 🐰" + " " + name, mentions: arraytag})}, 7000);
